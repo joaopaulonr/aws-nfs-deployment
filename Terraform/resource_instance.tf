5bd2614cca69fa713bfc463e210a7162d23fdf0c
João Paulo N. Rodrigues <joaopaulonr.ti@gmail.com>
2023-09-26 15:28:40 -0300

    alteração AMI

--- Terraform/resource_instance.tf
+++ Terraform/resource_instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "linux_instance" {
-    ami = "ami-03a6eaae9938c858c"
+    ami = "ami-00c6177f250e07ec1"
     instance_type = "t3.small"
     key_name = "vockey"
     subnet_id = aws_subnet.linux_subnet_public.id

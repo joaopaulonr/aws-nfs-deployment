d8e612dae5197ccf8dc806d65431b918811507e3
João Paulo N. Rodrigues <joaopaulonr.ti@gmail.com>
2023-09-29 16:43:08 -0300

    adição da instância cliente

--- Terraform/resource_instance.tf
+++ Terraform/resource_instance.tf
@@ -18,3 +18,24 @@ resource "aws_instance" "linux_instance" {
       CostCenter = "C092000004"
     }
 }
+
+resource "aws_instance" "client_instance" {
+    ami = "ami-00c6177f250e07ec1"
+    instance_type = "t2.micro"
+    key_name = "vockey"
+    subnet_id = aws_subnet.linux_subnet_public.id
+    vpc_security_group_ids = [aws_security_group.linux_security_group.id]
+    associate_public_ip_address = true
+    user_data = "${file("../Scripts/client.sh")}"
+    
+    root_block_device {
+      volume_size = 8
+      volume_type = "gp3"
+    }
+    
+    tags = {
+      Name = "linux-nfs-nginx client"
+      Project = "PB UFC"
+      CostCenter = "C092000004"
+    }
+}

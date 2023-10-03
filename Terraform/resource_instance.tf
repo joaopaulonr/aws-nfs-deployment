7367dd096e0a22c5e9d2fe5e1d7a18497a058048
João Paulo N. Rodrigues <joaopaulonr@gmail.com>
2023-10-03 10:33:39 -0300

    inserção do template de tags

--- Terraform/resource_instance.tf
+++ Terraform/resource_instance.tf
@@ -1,41 +1,37 @@
 resource "aws_instance" "linux_instance" {
     ami = "ami-00c6177f250e07ec1"
     instance_type = "t3.small"
-    key_name = "vockey"
+    key_name = "linux"
     subnet_id = aws_subnet.linux_subnet_public.id
     vpc_security_group_ids = [aws_security_group.linux_security_group.id]
     associate_public_ip_address = true
     user_data = "${file("../Scripts/service.sh")}"
     
+    launch_template {
+      id = aws_launch_template.linux_template.id
+    }
+    
     root_block_device {
       volume_size = 16
       volume_type = "gp3"
-    }
-    
-    tags = {
-      Name = "linux-nfs-nginx"
-      Project = "PB UFC"
-      CostCenter = "C092000004"
     }
 }
 
 resource "aws_instance" "client_instance" {
     ami = "ami-00c6177f250e07ec1"
     instance_type = "t2.micro"
-    key_name = "vockey"
+    key_name = "linux"
     subnet_id = aws_subnet.linux_subnet_public.id
     vpc_security_group_ids = [aws_security_group.client_security_group.id]
     associate_public_ip_address = true
     user_data = "${file("../Scripts/client.sh")}"
     
+    launch_template {
+      id = aws_launch_template.linux_template.id
+    }
+
     root_block_device {
       volume_size = 8
       volume_type = "gp3"
-    }
-    
-    tags = {
-      Name = "linux-nfs-nginx_client"
-      Project = "PB UFC"
-      CostCenter = "C092000004"
     }
 }
